cfec00d5c225ac9c0a7ce353990cb1e1c80f212b
Omarcito <Omar@omardearmas.com>
2013-05-27 14:11:41 -0700

    Attempt at Magnific modals for article images. (not working)

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,39 @@
 
+
+$(document).ready(function() {
+
+  $('.image-popup-vertical-fit').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-img-mobile',
+    image: {
+      verticalFit: true
+    }
+  });
+
+  $('.image-popup-fit-width').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    image: {
+      verticalFit: false
+    }
+  });
+
+  $('.image-popup-no-margins').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    mainClass: 'mfp-no-margins', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    }
+  });
+
+});
+
+
+
+
 //---------------Stick Nav to top of page on scroll---------------//
 // $(window).scroll(function(e){
 //   var navtop = $('.sticky_js').offset().top;
